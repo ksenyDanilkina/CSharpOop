c4d2baf2686043df5096a4cf7c0d18520524418e
Ксения Данилкина <hostel1992@mail.ru>
2020-07-06 22:58:25 +0600

    Добавлен проект CSharpOop.RangeTask. Реализовано нахождение интервала-пересечения.

--- CSharpOop/Program.cs
+++ CSharpOop/Program.cs
@@ -4,13 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpOop
+namespace CSharpOop.RangeTask
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Введите начало числового диапазона: ");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конец числового диапазона: ");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите начало второго числового диапазона: ");
+            double from2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конец второго числового диапазона: ");
+            double to2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число, для проверки принадлежности диапазону: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Range range = new Range(from, to);
+            Range range2 = new Range(from2, to2);
+
+            Console.WriteLine("Начало диапазона: " + from);
+            Console.WriteLine("Конец диапазона: " + to);
+            Console.WriteLine("Длина диапазона: " + range.GetLength());
+            Console.WriteLine("Число принадлежит диапазону: " + range.IsInside(number));
+
+            Range intersectionInterval = range.GetIntersectionInterval(range2);
+
+            if (intersectionInterval == null)
+            {
+                Console.WriteLine("Пересечений нет.");
+            }
+            else
+            {
+                Console.WriteLine("Интервал - пересечение: ( "+intersectionInterval.From +" ; "+intersectionInterval.To+" )");
+            }
         }
     }
 }
---
+++ CSharpOop/Range.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOop.RangeTask
+{
+    class Range
+    {
+        public double From
+        {
+            get;
+            set;
+        }
+
+        public double To
+        {
+            get;
+            set;
+        }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        public Range GetIntersectionInterval(Range range2)
+        {
+            double from;
+            double to;
+
+            if ((range2.From - To) * (range2.To - From) >= 0)
+            {
+                return null;
+            }
+
+            if (range2.From > From)
+            {
+                from = range2.From;
+            }
+            else
+            {
+                from = From;
+            }
+
+            if (range2.To > To)
+            {
+                to = To;
+            }
+            else
+            {
+                to = range2.To;
+            }
+
+            return new Range(from, to);
+        }
+    }
+}
