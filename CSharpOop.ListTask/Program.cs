ec50405449fadfa22e0c45be03f65f1f07b7c1dd
Ксения Данилкина <hostel1992@mail.ru>
2020-08-24 17:11:43 +0600

    Added ListTask

---
+++ CSharpOop.ListTask/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOop.ListTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList<int> list = new SinglyLinkedList<int>();
+
+            list.AddFirst(3);
+            list.AddFirst(4);
+            list.AddFirst(7);
+            list.AddFirst(9);
+            list.AddFirst(0);
+
+            list.AddByIndex(1, 55);
+
+            Console.WriteLine("Размер списка: " + list.GetSize());
+            Console.WriteLine("Удаленный первый элемент: " + list.GetRemovedFirstElement());
+            Console.WriteLine("Значение первого элемента: " + list.GetFirstElementData());
+            Console.WriteLine("Измененный элемент: " + list.GetChangedElementData(3, 50));
+            Console.WriteLine("Удаленный элемент: " + list.GetRemovedElementData(2));
+            Console.WriteLine("Элемент удален: " + list.IsRemoved(2));
+
+            list.Revert();
+            Console.WriteLine("Перевернутый список: " + PrintList(list));
+
+            SinglyLinkedList<int> copyList = list.GetCopy();
+            Console.WriteLine("Копия списка: " + PrintList(copyList));
+        }
+
+        public static string PrintList(SinglyLinkedList<int> list)
+        {
+            StringBuilder resultString = new StringBuilder();
+
+            for (int i = 0; i < list.GetSize(); i++)
+            {
+                if (i < list.GetSize() - 1)
+                {
+                    resultString.Append(list.GetElementData(i)).Append(", ");
+                }
+                else
+                {
+                    resultString.Append(list.GetElementData(i));
+                }
+            }
+
+            return resultString.ToString();
+        }
+    }
+}
---
+++ CSharpOop.ListTask/SinglyLinkedList.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+namespace CSharpOop.ListTask
+{
+    class SinglyLinkedList<T>
+    {
+        private ListItem<T> head;
+        private int count;
+
+        public int GetSize()
+        {
+            return count;
+        }
+
+        public T GetFirstElementData()
+        {
+            return head.Data;
+        }
+
+        public void AddFirst(T data)
+        {
+            ListItem<T> listItem = new ListItem<T>(data, head);
+            head = listItem;
+
+            count++;
+        }
+
+        public T GetRemovedFirstElement()
+        {
+            T removedElement = head.Data;
+            head = head.Next;
+            count--;
+
+            return removedElement;
+        }
+
+        public T GetElementData(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index не может быть отрицательным", nameof(index));
+            }
+
+            if (index > count - 1)
+            {
+                throw new ArgumentException("index выходит за границы списка", nameof(index));
+            }
+
+            if (index == 0)
+            {
+                return head.Data;
+            }
+
+            ListItem<T> currentElement = head;
+
+            for (int i = 0; i < index; i++)
+            {
+                currentElement = currentElement.Next;
+            }
+
+            return currentElement.Data;
+        }
+
+        public T GetChangedElementData(int index, T data)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index не может быть отрицательным", nameof(index));
+            }
+
+            if (index > count - 1)
+            {
+                throw new ArgumentException("index выходит за границы списка", nameof(index));
+            }
+
+            ListItem<T> currentElement = head;
+
+            for (int i = 0; i < index; i++)
+            {
+                currentElement = currentElement.Next;
+            }
+
+            T changedElementData = currentElement.Data;
+            currentElement.Data = data;
+
+            return changedElementData;
+        }
+
+        public T GetRemovedElementData(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index не может быть отрицательным", nameof(index));
+            }
+
+            if (index > count - 1)
+            {
+                throw new ArgumentException("index выходит за границы списка", nameof(index));
+            }
+
+            if (index == 0)
+            {
+                return GetRemovedFirstElement();
+            }
+
+            ListItem<T> currentElement = head;
+
+            for (int i = 0; i < index - 1; i++)
+            {
+                currentElement = currentElement.Next;
+            }
+
+            T removedElementData = currentElement.Next.Data;
+            currentElement.Next = currentElement.Next.Next;
+            count--;
+
+            return removedElementData;
+        }
+
+        public void AddByIndex(int index, T data)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index не может быть отрицательным", nameof(index));
+            }
+
+            if (index > count - 1)
+            {
+                throw new ArgumentException("index выходит за границы списка", nameof(index));
+            }
+
+            if (index == 0)
+            {
+                AddFirst(data);
+            }
+            else
+            {
+                ListItem<T> currentElement = head;
+
+                for (int i = 0; i < index - 1; i++)
+                {
+                    currentElement = currentElement.Next;
+                }
+
+                ListItem<T> elementForAdd = new ListItem<T>(data, currentElement.Next);
+                currentElement.Next = elementForAdd;
+                count++;
+            }
+        }
+
+        public bool IsRemoved(T data)
+        {
+            for (ListItem<T> p = head, prev = null; p != null; prev = p, p = p.Next)
+            {
+                if (p.Data.Equals(data))
+                {
+                    if (prev == null)
+                    {
+                        GetRemovedFirstElement();
+                    }
+                    else
+                    {
+                        prev.Next = p.Next;
+                        count--;
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Revert()
+        {
+            ListItem<T> previousElement = null;
+
+            for (ListItem<T> p = head; p != null;)
+            {
+                ListItem<T> nextElement = p.Next;
+                p.Next = previousElement;
+                previousElement = p;
+                p = nextElement;
+            }
+
+            head = previousElement;
+        }
+
+        public SinglyLinkedList<T> GetCopy()
+        {
+            SinglyLinkedList<T> copiedList = new SinglyLinkedList<T>();
+
+            for (ListItem<T> p = head; p != null; p = p.Next)
+            {
+                copiedList.AddFirst(p.Data);
+            }
+
+            copiedList.Revert();
+
+            return copiedList;
+        }
+    }
+}
